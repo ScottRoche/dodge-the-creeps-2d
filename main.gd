fbe0fd2c07fb8d640d43dcd57a9e610e1fb916ba
Scott Roche <scott.roche24@gmail.com>
2024-07-30 20:37:32 +0100

    Implement HUD and start/end game
    Implements the HUD to display score info, buttons and other such info.

--- main.gd
+++ main.gd
@@ -7,7 +7,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,10 +19,17 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 
+	$HUD.show_game_over()
+
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
+
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -53,6 +60,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
